7011cb43ccbbfbe61a13e1bf0722bb8853deb292
SzczupakKrolemPontaru <szymonwydmuch1@gmail.com>
2024-11-18 22:00:35 +0100

    First commit

---
+++ VibeCheckAI-Logic/src/main/java/com/example/vibecheckai/VibecheckaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.vibecheckai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibecheckaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibecheckaiApplication.class, args);
+	}
+
+}
