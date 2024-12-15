ee3d6cba509421db9fcab45509d85af28bc7f85b
SzczupakKrolemPontaru <szymonwydmuch1@gmail.com>
2024-12-15 04:08:54 +0100

    Renamed folder

--- VibeCheckAI-Model/config/modelPreparations.py
+++ VibeCheckAI-Model/config/modelPreparations.py
@@ -5,17 +5,17 @@ from onnxruntime.quantization import quantize_dynamic, QuantType
 
 MODEL_NAME = "cardiffnlp/twitter-xlm-roberta-base-sentiment-multilingual"
 
-ORYGINAL_MODEL_DIRECTORY_PATH = "./onnx_model"
-ORYGINAL_MODEL_PATH = "./onnx_model/model.onnx"
+ORYGINAL_MODEL_DIRECTORY_PATH = "./models/onnx_model"
+ORYGINAL_MODEL_PATH = "./models/onnx_model/model.onnx"
 
-OPTIMIZED_MODEL_DIRECTORY_PATH = "./optimized_onnx_model"
-OPTIMIZED_MODEL_PATH = "./optimized_onnx_model/model_optimized.onnx"
+OPTIMIZED_MODEL_DIRECTORY_PATH = "./models/optimized_onnx_model"
+OPTIMIZED_MODEL_PATH = "./models/optimized_onnx_model/model_optimized.onnx"
 
-QUANTIZED_MODEL_DIRECTORY_PATH = "./quantized_onnx_model"
-QUANTIZED_MODEL_PATH = "./quantized_onnx_model/quantized_model.onnx"
+QUANTIZED_MODEL_DIRECTORY_PATH = "./models/quantized_onnx_model"
+QUANTIZED_MODEL_PATH = "./models/quantized_onnx_model/quantized_model.onnx"
 
-# model = ORTModelForSequenceClassification.from_pretrained(MODEL_NAME, export=True)
-# model.save_pretrained(ORYGINAL_MODEL_DIRECTORY_PATH)
+model = ORTModelForSequenceClassification.from_pretrained(MODEL_NAME, export=True)
+model.save_pretrained(ORYGINAL_MODEL_DIRECTORY_PATH)
 
 optimizer = ORTOptimizer.from_pretrained(ORYGINAL_MODEL_DIRECTORY_PATH)
 optimization_config = OptimizationConfig(optimization_level=2)
